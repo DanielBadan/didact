d554c38a8b74299052bab00cdd12a4512e133631
Daniel Verejan <daniel@Daniels-MacBook-Pro.local>
2018-07-22 20:04:49 +0300

    Add createElement function for JSX support

---
+++ src/createElement.js
@@ -0,0 +1,17 @@
+const TEXT_ELEMENT = "TEXT ELEMENT";
+
+export function createElement(type, config, ...args) {
+	const props = Object.assign({}, config);
+	const hasChildren = args.length > 0;
+	const rawChildren = hasChildren ? [].concat(...args) : [];
+
+	props.children = rawChildren
+		.filter(c => c != null && c!== false)
+		.map(c => c instanceOf Object ? c : createTextElement(c));
+
+	return { type, props };
+}
+
+function createTextElement(value) {
+	return createElement(TEXT_ELEMENT, { node_value: value });
+}
